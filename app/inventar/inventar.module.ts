1aa0fb6e72607c9d9390c44d4158ab7f23b1a49b
Dorian Nyffeler <dorian.nyffeler@gmail.com>
2017-02-18 15:29:33 +0100

    ngx-datamodule import

--- app/inventar/inventar.module.ts
+++ app/inventar/inventar.module.ts
@@ -6,10 +6,11 @@ import { InventarComponent } from './inventar.component';
 import { ArtikelComponent } from './artikel.component';
 import { InventarlisteComponent } from './inventarliste.component';
 import { InventarRoutingModule } from './inventar-routing.module';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 
 @NgModule({
-  imports: [SharedModule, InventarRoutingModule],
+  imports: [SharedModule, InventarRoutingModule, NgxDatatableModule],
   declarations: [
     InventarComponent, ArtikelComponent, InventarlisteComponent,
   ]
--- app/inventar/inventarliste.component.ts
+++ app/inventar/inventarliste.component.ts
@@ -1,17 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 import {
   Artikel,
   InventarService
 } from './inventar.service';
 
+
 @Component({
   templateUrl: './app/inventar/inventarliste.component.html'
 })
 
+
 export class InventarlisteComponent implements OnInit {
+  rows = [
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Molly', gender: 'Female', company: 'Burger King' },
+  ];
+  columns = [
+    { prop: 'name' },
+    { name: 'Gender' },
+    { name: 'Company' }
+  ];
+
   inventarliste: Promise<Artikel[]>;
   constructor(private inventarService: InventarService) { }
+
 
   ngOnInit() {
     this.inventarliste = this.inventarService.getInventarliste();
